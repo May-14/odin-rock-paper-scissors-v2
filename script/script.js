719b3d0adb0b8ccaef13b6153bbc134435423328
Matias <26matmoran@gmail.com>
2025-02-25 11:04:54 -0600

    wrote a function to get user input

--- script/script.js
+++ script/script.js
@@ -8,3 +8,20 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = parseInt(prompt("Type 0 to choose paper, 1 to choose rock and 2 to choose scissors: "));
+    while (humanChoice !== 0 && humanChoice !== 1 && humanChoice !== 2) {
+        humanChoice = parseInt(prompt("Please type 0 to choose paper, 1 to choose rock and 2 to choose scissors: "));
+    }
+    if (humanChoice === 0) {
+        return "paper";
+    } else if (humanChoice === 1) {
+        return "rock";
+    } else {
+        return "scissors";
+    }
+    return humanChoice;
+}
+
+console.log(getHumanChoice())
