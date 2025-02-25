1ded00fa585d9f6aa8e3dca66128c2ec72de1c12
Matias <26matmoran@gmail.com>
2025-02-25 11:20:33 -0600

    readd score variables and change function logic
    every calculation is now done with numbers

--- script/script.js
+++ script/script.js
@@ -1,25 +1,39 @@
+// through the whole code, paper is 1, scissors is 2, and rock is 3
+
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1
-    if (randomNumber === 3) {
-        return "paper";
-    } else if (randomNumber === 2) {
-        return "rock";
-    } else {
-        return "scissors";
-    }
+    return Math.floor(Math.random() * 3) + 1;
 }
 
 function getHumanChoice() {
-    let humanChoice = parseInt(prompt("Type 0 to choose paper, 1 to choose rock and 2 to choose scissors: "));
-    while (humanChoice !== 0 && humanChoice !== 1 && humanChoice !== 2) {
-        humanChoice = parseInt(prompt("Please type 0 to choose paper, 1 to choose rock and 2 to choose scissors: "));
+    let humanChoice = parseInt(prompt("Type 1 to choose paper, 2 to choose scissors and 3 to choose rock: "));
+    while (humanChoice !== 1 && humanChoice !== 2 && humanChoice !== 3) {
+        humanChoice = parseInt(prompt("Please type 1 to choose paper, 2 to choose scissors and 3 to choose rock: "));
     }
-    if (humanChoice === 0) {
-        return "paper";
-    } else if (humanChoice === 1) {
-        return "rock";
-    } else {
-        return "scissors";
-    }
+    return humanChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice > computerChoice) {
+        if (humanChoice - computerChoice === 2) {
+            computerScore += 1
+            return "you lost"
+        } else {
+            humanScore += 1
+            return "you won"
+        }
+    } else if (computerChoice > humanChoice) {
+        if (computerChoice - humanChoice === 2) {
+            humanScore += 1
+            return "you won"
+        } else {
+            computerScore += 1
+            return "you lost"
+        }
+    } else {
+        return "tie"
+    }
+}
